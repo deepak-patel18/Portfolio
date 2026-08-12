0456613dc5f4f22457313e5601475ccbcaa33241
Deepak_Patel18 <deepakpp649@gmail.com>
2026-08-12 12:43:34 +0530

    Fix Render server port

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,22 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 
 dotenv.config();
-connectDB();
 
 const app = express();
 
+// Connect MongoDB
+connectDB();
+
 // Middleware
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://portfolio-six-hazel-n1fqu65lpu.vercel.app"
-  ],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://portfolio-six-hazel-n1fqu65lpu.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
@@ -24,12 +28,14 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/contact", require("./routes/contactRoutes"));
 
+// Test route
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
+// Port
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
